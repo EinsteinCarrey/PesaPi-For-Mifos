33163052195a330ff784bc79422664fbe6fe2092
EinsteinCarrey <forensicslabnet@gmail.com>
2017-09-09 12:14:30 +0300

    [UPDATE] Implements transaction charges for mpesa transactions

--- application/controllers/Core.php
+++ application/controllers/Core.php
@@ -16,7 +16,7 @@ class Core extends CI_Controller {
         if(ENVIRONMENT == "production"){
             $data =  $this->MpesaClientHandler_->getPesaPiPostedData();
         }else{
-            $data['receipt']= "test_receipt";
+            $data['receipt']= "test_receipt_".rand(10,100000);
             $data['type'] = 6;
             $data['time'] = time();
             $data['phoneNumber'] = '0707070707';
@@ -29,27 +29,52 @@ class Core extends CI_Controller {
         }
 
         if($this->MpesaClientHandler_->dataHasErrors($data)) {
-            print_r($this->errorMessage);
+            //ToDo: Log error messages
+            // $this->errorMessage;
             die();
         }
 
+        # get clients ID
         $client = $this->MpesaClientHandler_->findClientByPhoneNumber($data['phoneNumber']);
         $data['clientID'] = $client->entityId;
 
+        # check if client has an active loan
         $clientHasAnActiveLoan = $this->MpesaClientHandler_->clientHasActiveLoanAccount($data);
 
+        # Deduct (Transaction charges) Ksh 5/= from the amount posted by client
+        $transaction_charge = 5;
+        $amount_posted = $data['amount'];
+        $amount_after_deduction = ($amount_posted - $transaction_charge);
+        $data['amount'] = $amount_after_deduction;
+
         if($clientHasAnActiveLoan){
+            # Make repayments
             $outPut = $this->MpesaClientHandler_->makeRepaymentToALoanAccount($data);
+
         }else{
+            # Deposit to savings account
             $outPut = $this->MpesaClientHandler_->makeDepositToClientSavingsAccount($data);
+
         }
+
+        print_r($outPut);
 
         //Confirm That amount has been posted successfully
         if(array_key_exists('resourceId',$outPut['result'])){
 
+            # record transaction in Mpesa register
             $this->LocalDBHandler->recordTransactionThatHaveBeenPostedToMifosDatabase($outPut['data']);
-            $data['messageBody'] = "Confirmed.\r\n Amount of Ksh " . $data['amount'] . " has been credited to your Jambo Account.";
+
+            # send a message to clients
+            $data['messageBody'] = "Confirmed. Ksh " .
+                $data['amount']+$transaction_charge . " has been received. to your Jambo Account.";
             $this->MpesaClientHandler_->sendMessageToClient($data);
+
+            # Deposit Ksh 5/= to BulkSMS charges account
+            $transaction_data = $data;
+            $transaction_data['amount'] = 5;
+            $transaction_data['clientID'] = 7879;
+            $this->MpesaClientHandler_->makeDepositToClientSavingsAccount($transaction_data);
         }
 
     }
